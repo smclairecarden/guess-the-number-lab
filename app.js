b6dc7a0d078f2d62c9f8928c680058f50c4fff8a
Claire Carden <smclairecarden@gmail.com>
2022-01-28 21:16:48 -0600

    complete through 3

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 let prevGuesses = []
 
 
-
 const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
@@ -12,5 +11,28 @@ const game = {
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      return this.secretNum
   }
 }
+
+console.log(game.play())
+
+let  small = this.smallestNum
+let big = this.biggestNum
+let playerValue = 45
+
+
+
+
+function getGuess(small, big) {
+  return `Enter a guess between ${small} and ${big}: ${playerValue}: `
+}
+
+
+console.log(getGuess(1, 100))
+
+do {
+  game.play
+} while(playerValue >= this.smallestNum || playerValue <= this.biggestNum)
+
+
